34783d261a43065b23f6f7d012f267c5591ab70f
ryudiDEV <123523187+ryudi1234@users.noreply.github.com>
2024-01-15 16:51:51 -0300

    Update personagem.js
    a

--- personagem.js
+++ personagem.js
@@ -1,19 +1,76 @@
 class Personagem{
-    constructor(nome, hp, classe){
+    constructor(nome, idade, tipo){
       this.nome = nome
-      this.classe = classe
-      this.hp = hp
+      this.tipo = tipo
+      this.idade = idade
     }
   }
   
-  class Mago extends Personagem {
-      constructor(nome, hp, classe, mana){
-        super(nome, hp, classe)
+    class Mago extends Personagem {
+      constructor(nome, idade, tipo, mana){
+        super(nome, idade, tipo)
         this.mana = mana
       }
-    lancarFeitico(nomeFeitico){
-      console.log(this.nome+ "lançou um feitico" + nomeFeitico )
-    }
+      ataque()
+      {
+        console.log(`o ${this.tipo} (usou magia)`)
+      }
   }
+
   
+  let pessoa = new Mago("siclano", 30,  "mago", 3000)
+
   
+
+  class Guerreiro extends Personagem {
+    constructor(nome, hp, tipo, forca){
+      super(nome, hp, tipo)
+      this.forca = forca
+    }
+
+    ataque()
+  {
+    console.log(`o ${this.tipo}  (usou espada)`)
+  }
+}
+
+
+
+
+class Monge extends Personagem {
+  constructor(nome, hp, tipo, lutador){
+    super(nome, hp, tipo)
+    this.lutador = lutador
+  }
+  ataque()
+  {
+    console.log(`o ${this.tipo}  (usou artes marciais)`)
+  }
+}
+
+
+
+class Ninja extends Personagem {
+  constructor(nome, hp, tipo, furtivo){
+    super(nome, hp, tipo)
+    this.furtivo = furtivo
+  }
+  ataque()
+  {
+    console.log(`o ${this.tipo} (usou shuriken)`)
+  }
+
+}
+
+
+let mago = new Mago("fulano", 30,  "guerreiro", 400) 
+Mago.ataque()
+
+let guerreiro = new Guerreiro("fulano", 30,  "guerreiro", 400) 
+Guerreiro.ataque()
+
+let monge = new Monge("Okuzōin Dōei", 30, "monge", "artista marcial")
+Monge.ataque()
+
+let ninja = new Ninja("Hattori Hanzo", 30, "ninja", "furtivo")
+Ninja.ataque
